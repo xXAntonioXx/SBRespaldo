6b5a246d6ef3a487ad555000ae630f3bdef55bf4
antonio gonzalez <Antonio Gonzalez>
2018-05-14 00:19:07 -0700

    proyecto final 85% no se pudo escribir en archivo y falta encontrar errores logicos

--- proyectoFinal/instruccion.asm
+++ proyectoFinal/instruccion.asm
@@ -8,10 +8,24 @@ section .data
     sangria db "   ",0x0
     sangria2 db "                       ",0x0
 
+    archivo db "este es un archivo.txt",0x0
+    mensajePrueba db "y este es el mensaje de prueba",0x0
+    segundoMensaje db "este es otro mensaje",0x0
+    longitud equ $-mensajePrueba
+    segundaLong equ $-segundoMensaje
+
 section .bss
     numeroCapt resb 4
     numeroCaptLen equ $-numeroCapt
     numero_recivido resb 4
+
+    CadenaImprimir resb 1024
+    CadenaFinal resb 1024
+    CadenaImprimirLen equ $-CadenaImprimir 
+    CadenaFinallong equ $-CadenaFinal
+
+    arregloStream resb 1024
+    longitudStream equ $-arregloStream
 
 section .text
 agregarDato:
@@ -89,7 +103,54 @@ mostrarDatos:
 	call bucle
 
 guardarArchivo:
-    call salida
+    ;primero convertir a cadena cada numero
+    ;mov ebx,CadenaFinal    ;cadena Final donde se tiene los numeros y resultados
+    mov edx,CadenaFinal
+    mov ecx,0
+    mov esi,CadenaImprimir ;esta cadena funciona como auxiliar para coonvertir a numero
+    mov ebp,resultadosArg      ;obtener el buffer con los numeros
+    call sacarNumeros
+    mov edx,CadenaFinal
+    mov eax,edx
+    call sprintLF
+    push eax
+    push ebx
+    push ecx
+    ;esto crea el archivo
+    mov eax,sys_creat
+    mov ebx,archivo
+    mov ecx,511
+    int 80h
+    ;aqui se abre el archivo
+    mov eax,sys_open
+    mov ebx,archivo
+    mov ecx,O_RDWR
+    int 80h
+    mov ebx,eax
+    ;y esto escribe en el archivo
+    mov eax,sys_write
+    mov edx,CadenaFinallong
+    mov ecx,CadenaFinal
+    int 80h
+    ;ahora sincronizar
+    mov eax,sys_sync
+    int 80h
+
+    ;finalmente se cierra el archivo
+    mov eax,sys_close
+    int 80h
+
+    cmp eax,0
+    jle salida
+
+    mov ebx,eax
+
+    ;ya terminada la apertura
+    pop ecx
+    pop ebx
+    pop eax
+
+    call mostrarMenu
 
 
 ;todo este bucle pertenece a imprimir resultado
@@ -127,31 +188,82 @@ glOperacion:                                                        ;
     jmp glBucle                                                     ;
 ;===================================================================
 
-;todo este bloque para generarCurva
-gcBucle:
+;todo este bloque para generarCurva=================================
+gcBucle:                                                            ;
+    cmp ecx,edi                                                     ; 
+    jl gcOperacion                                                  ;
+    ret                                                             ;
+gcOperacion:                                                        ;
+    mov ebx,[esi+ecx*4];primero un auxiliar del valor               ;
+    mov eax,ebx                                                     ;
+    ;primero la cubica                                              ;
+    imul eax,eax                                                    ;
+    imul eax,ebx                                                    ;
+    ;en eax va el cubico                                            ;
+    ;primero cuadrado                                               ;
+    mov edx,ebx                                                     ;
+    imul edx,edx                                                    ;
+    imul edx,4 ;valor del segundo elemento en ecx                   ;
+                                                                    ;
+    ;finalmente ebx se convierte en el ultimo elemento              ;
+    imul ebx,6                                                      ;
+                                                                    ;
+    sub eax,edx                                                     ;
+    add eax,ebx                                                     ;
+    sub eax,24 ;en eax esta el resultado final                      ;
+                                                                    ;
+    mov [ebp+ecx*4],eax;guardamos el resultado                      ;
+                                                                    ;
+    inc ecx                                                         ;
+    jmp gcBucle                                                     ;
+;===================================================================
+
+;todo este bloque pertenece a guardar el archivo
+sacarNumeros:
     cmp ecx,edi
-    jl gcOperacion
+    jl convertir
     ret
-gcOperacion:
-    mov ebx,[esi+ecx*4];primero un auxiliar del valor
-    mov eax,ebx
-    ;primero la cubica
-    imul eax,eax
-    imul eax,ebx
-    ;en eax va el cubico
-    ;primero cuadrado
-    mov edx,ebx 
-    imul edx,edx
-    imul edx,4 ;valor del segundo elemento en ecx
-
-    ;finalmente ebx se convierte en el ultimo elemento
-    imul ebx,6 
-
-    sub eax,edx
-    add eax,ebx
-    sub eax,24 ;en eax esta el resultado final
-
-    mov [ebp+ecx*4],eax;guardamos el resultado
+convertir:
+    mov eax,[ebp+ecx*4]
+    call itoa
+    ;cmp ebx,0
+    ;jg after
+    
+    mov eax,esi
+    ;push esi
+    ;mov esi,arregloStream
+    ;call copystring
+    mov [edx+ebx],eax
+    ;pop esi
+    push eax
+    call strlen
+    add ebx,eax
+    pop eax
+    call sprintLF
 
     inc ecx
-    jmp gcBucle
+    mov eax,ebx
+    call iprintLF
+    jmp sacarNumeros
+
+;after:
+;   mov eax,esi
+;   push eax
+;   call strlen
+;   add ebx,eax
+;   pop eax
+;   call sprintLF
+;   inc ecx
+;   jmp sacarNumeros
+
+;abrirArchivo:
+ ;   mov eax,sys_open
+  ;  mov ebx,archivo
+   ; mov ecx,O_RDWR
+    ;int 80h
+
+    ;cmp eax,0
+    ;jle salida
+
+    ;mov ebx,eax
+    ;ret
